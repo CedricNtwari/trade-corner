da0da543ac5249b1fff7e9fe73f2d32d4bb654ce
CedricNtwari <cedric.ntwari@powercoders.org>
2024-08-13 11:04:28 +0200

    feat: Implement responsive footer layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import styles from './App.module.css'
+import Footer from './components/Footer'
 import NavBar from './components/NavBar'
 
 function App() {
   return (
     <div className={styles.App}>
       <NavBar />
+      <div className={styles.content}>{/* Your main content goes here */}</div>
+      <Footer />
     </div>
   )
 }
