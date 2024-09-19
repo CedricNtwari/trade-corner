75e5f099e627a1e2a170c7575af1f99c3512d366
CedricNtwari <cedric.ntwari@powercoders.org>
2024-09-19 12:29:49 +0200

    feat: add routes for ProductsPage and ProfilePage

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import { Route, Switch } from 'react-router-dom'
 import './api/axiosDefaults'
 import SignUpForm from './pages/auth/SignUpForm'
 import SignInForm from './pages/auth/SignInForm'
+import ProductsPage from './pages/products/ProductsPage'
+import ProfilePage from './pages/profiles/ProfilePage'
 
 function App() {
   return (
@@ -17,6 +19,8 @@ function App() {
           <Route exact path="/signin" render={() => <SignInForm />} />
           <Route exact path="/signup" render={() => <SignUpForm />} />
           <Route exact path="/cart" render={() => <h1>Shopping Cart</h1>} />
+          <Route exact path="/products" component={ProductsPage} />
+          <Route path="/profiles/:id" component={ProfilePage} />
           <Route render={() => <p>Page not found!</p>} />
         </Switch>
       </Container>
