f46b4d2f14820e9726b20c2b4b8115cb288a2759
CedricNtwari <cedric.ntwari@powercoders.org>
2024-10-13 21:35:59 +0200

    test: add setup for testing environment

--- src/setupTests.js
+++ src/setupTests.js
@@ -2,4 +2,21 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+// jest-dom adds custom jest matchers for asserting on DOM nodes.
+// allows you to do things like:
+// expect(element).toHaveTextContent(/react/i)
+// learn more: https://github.com/testing-library/jest-dom
+import '@testing-library/jest-dom'
+import { setupServer } from 'msw/node'
+import { handlers } from './mocks/handlers'
+
+const server = setupServer(...handlers)
+
+// Start server before all tests
+beforeAll(() => server.listen())
+
+// Reset any request handlers that we may alter in our tests
+afterEach(() => server.resetHandlers())
+
+// Close server after all tests
+afterAll(() => server.close())
