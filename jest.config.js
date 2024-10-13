fdf8186d96b46172e55a2491955a3690624df69e
CedricNtwari <cedric.ntwari@powercoders.org>
2024-10-13 21:33:17 +0200

    test: update Jest configuration for file extensions and directories

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,8 @@ module.exports = {
   transform: {
     '^.+\\.(js|jsx)$': 'babel-jest',
   },
+  moduleFileExtensions: ['js', 'jsx', 'json', 'node'],
+  moduleDirectories: ['node_modules', 'src'],
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|svg)$': '/Users/cedric/trade-corner/fileMock.js',
     '\\.(css|less|scss|sss|styl)$': 'identity-obj-proxy',
