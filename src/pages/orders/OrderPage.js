3e646315378babb68f0ff63fced604e8510018e0
CedricNtwari <cedric.ntwari@powercoders.org>
2024-10-18 08:17:10 +0200

    fix(auth): ensure order history is only fetched when user is authenticated

--- src/pages/orders/OrderPage.js
+++ src/pages/orders/OrderPage.js
@@ -3,11 +3,15 @@ import axios from 'axios'
 import styles from '../../styles/OrderPage.module.css'
 import LoadingSpinner from '../../components/LoadingSpinner'
 import OrdersList from './OrdersList'
+import { useCurrentUser } from '../../contexts/CurrentUserContext'
+import { useHistory } from 'react-router-dom/cjs/react-router-dom.min'
 
 const OrderPage = () => {
   const [orders, setOrders] = useState([])
   const [loading, setLoading] = useState(true)
   const [error, setError] = useState(null)
+  const currentUser = useCurrentUser()
+  const history = useHistory()
 
   useEffect(() => {
     const fetchOrders = async () => {
@@ -22,8 +26,12 @@ const OrderPage = () => {
       }
     }
 
-    fetchOrders()
-  }, [])
+    if (!currentUser) {
+      history.push('/signin')
+    } else {
+      fetchOrders()
+    }
+  }, [currentUser, history])
 
   if (loading) return <LoadingSpinner message="Loading your orders..." />
   if (error) return <p>{error}</p>
